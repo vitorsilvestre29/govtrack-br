3e5b67b03b95fb1ce20a520d71cc189c0f917a08
Vitor Silvestre <vitorcesarsilvestre2017@gmail.com>
2026-06-08 14:48:28 -0300

    feat: conexao python postgresql docker

---
+++ src/ingestion/database.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+def db_connect ():
+    return psycopg2.connect(host="localhost", database="govtrack", port=5432, user="govtrack", password="govtrack123")
+
+if __name__ == "__main__":
+    conn = db_connect()
+    print("Conexão com o banco de dados estabelecida com sucesso!")
+    conn.close()
+    
